0ef11e3a7bc8fd8f5f8070a4fcc89061e20780ab
hpotdar21@gmail.com <hpotdar21@gmail.com>
2023-03-30 19:09:15 +0530

    Updated commit

--- apitesting/lib/apitest_utils.ex
+++ apitesting/lib/apitest_utils.ex
@@ -1,8 +1,48 @@
-defmodule ApitestUtils do
- import HTTPoison
+defmodule   ApitestUtils do
+  import HTTPoison
+  require HTTPoison
 
- def doGet(url) do
-  {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url)
-  body
- end
+
+  def doGet(url) do
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url)
+    body
+  end
+
+
+  @spec get(String.t()) :: {:ok, integer, String.t()}
+  def get(url) do
+    {:ok, %HTTPoison.Response{status_code: status_code, body: body}} = HTTPoison.get(url)
+      {:ok, status_code, body}
+  end
+
+
+
+  @spec get(String.t()) :: {:ok, integer, String.t()}
+  def get(url, params \\ {} ) do
+    {:ok, %HTTPoison.Response{status_code: status_code, body: body}} = HTTPoison.get(url, params)
+      {:ok, status_code, body}
+  end
+
+
+
+ # @spec get(String.t()) :: {:ok, integer, String.t(),String.t()}
+  def get_req(url, headers \\ [] , params \\ {}) do
+    {:ok, %HTTPoison.Response{status_code: status_code, body: body, headers: headers}} = HTTPoison.get(url, headers, params)
+      {:ok, status_code, body, headers}
+  end
 end
+
+
+#require Logger
+
+#@spec get(String.t) :: {:ok, integer, String.t} | {:error, String.t}
+#def get(url) do
+#  case HTTPoison.get(url) do
+ #   {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+ #     {:ok, status_code, body}
+ #   {:error, %HTTPoison.Error{reason: reason}} ->
+ #     Logger.error("Error performing HTTP GET request: #{reason}")
+  #    {:error, reason}
+ # end
+#end
+#end
--- apitesting/test/employee_test.exs
+++ apitesting/test/employee_test.exs
@@ -4,7 +4,7 @@ defmodule EmployeeTest do
   #import HTTPoison
 
   _url = "https://reqres.in/api/users/2"
-
+  @tag content: false
   test "Simple Get" do
     url = "https://reqres.in/api/users/2"
     response = doGet(url)
@@ -12,4 +12,33 @@ defmodule EmployeeTest do
     {:ok, data} = Jason.decode(response)
     assert data["data"]["first_name"] == "Janet"
   end
+
+  @tag content: false
+  test "get body and status code" do
+    url = "https://reqres.in/api/users/2"
+    responce1 = get(url)
+    IO.puts(responce1 |> elem(1))
+    IO.puts(responce1 |> elem(2))
+    #{:ok, data} = Jason.decode(responce1)
+  end
+
+  @tag  content: true
+  test "send url with query parms" do
+    url = "https://reqres.in/api/users"
+    params =  %{page: 2}
+    res1 = get(url, params)
+    IO.puts(res1 |>  elem(1))
+    IO.puts(res1 |>  elem(2))
+  end
+
+  @tag content: true
+  test "send url,query parameter, headers" do
+    url = "https://reqres.in/api/users"
+    params =  %{page: 2}
+    header =  [{"Content-Type", "application/json;charset=utf-8"}]
+    res1 = get_req(url, params, header)
+    IO.puts(res1 |>  elem(1))
+    IO.puts(res1 |>  elem(2))
+  end
+
 end
